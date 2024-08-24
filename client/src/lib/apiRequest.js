457bd9539db5a1cecd4c9d525cf58164b7c3ae98
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-24 15:29:18 +0530

    uu76i6

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL: "https://catring-project-rebirth.onrender.com/api",
   withCredentials: true,
-});
+});   
 
 export default apiRequest;
