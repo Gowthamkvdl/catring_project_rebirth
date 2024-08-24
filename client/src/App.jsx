7154b8921638d52a58e52f94ed7e2e52c9fa36fb
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-24 15:36:55 +0530

    hhjgh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import UserProfile from "./routes/userProfilePage/UserProfile";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import PageNotFound from "./routes/pageNotFound/PageNotFound";
 import { listPageLoader, singlePageLoader } from "./lib/loader";
-import HomePage from "./routes/homePage/HomePage"; 
+import HomePage from "./routes/homePage/homePage"; 
 
 function App() {
   const router = createBrowserRouter([
