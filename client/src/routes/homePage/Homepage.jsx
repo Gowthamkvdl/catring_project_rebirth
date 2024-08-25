55ddbbab2c707529e5507c31c216a2b8755bb267
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-25 20:53:48 +0530

    fwegferh

--- client/src/routes/homePage/Homepage.jsx
+++ client/src/routes/homePage/Homepage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
 
   return (
     <div className="row homepage pt-3 pt-md-0">
-      <div className="col-12 col-md-7 my-sm-auto h-25 h-sm-auto mt-3">
+      <div className="col-12 col-md-7 h-25 h-sm-auto mt-3">
         <div className="hero-section mt-lg-0 mt-3 flex-column d-flex justify-content-center align-item-center">
           <div className="hero-text mb-3">
             <span className="title-text">
