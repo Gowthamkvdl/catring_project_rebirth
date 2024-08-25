71b753199dae333760ca8d191120446827cef3ee
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-25 22:19:11 +0530

    hfgtjf

--- client/src/routes/contactPage/ContactPage.jsx
+++ client/src/routes/contactPage/ContactPage.jsx
@@ -95,18 +95,16 @@ const ContactPage = () => {
           <div className="col-xl-6 mx-auto mt-sm-0 mt-4 col-12">
             <h4 className=" mb-2 d-flex ">
               Send us a message
-              <div className="rocket m-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="#0a5bd0"
-                  class="bi bi-chat me-1 mb-2"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
-                </svg>
-              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="#0a5bd0"
+                class="bi bi-chat me-1  mt-0"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
+              </svg>
             </h4>
             <form action="" onSubmit={onSubmit}>
               <div className="mb-3">
