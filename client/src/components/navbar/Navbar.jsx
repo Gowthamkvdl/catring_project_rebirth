ffbd79adff8f0b3e3046e12d0f184e43bc199f77
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-26 09:48:54 +0530

    hkjhl

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -655,7 +655,7 @@ const Navbar = () => {
                     <div className="role d-flex gap-3 mt-3">
                       <div className="form-check d-flex gap-2 justify-content-center align-items-center">
                         <input
-                          className="form-check-input "
+                          className="form-check-input mb-1"
                           type="radio"
                           name="role"
                           id="roleServer"
@@ -671,7 +671,7 @@ const Navbar = () => {
                       </div>
                       <div className="form-check d-flex gap-2 justify-content-center align-items-center">
                         <input
-                          className="form-check-input "
+                          className="form-check-input mb-1"
                           type="radio"
                           name="role"
                           id="roleCater"
