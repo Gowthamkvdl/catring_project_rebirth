9de96b044e37b2d283aa05a6d2d13951ee209bb2
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-26 10:05:18 +0530

    yfhujgfj

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -65,30 +65,16 @@ const Filter = ({ query, setQuery }) => {
           </svg>{" "}
           Show filters
         </a>
-        <div className=" d-flex align-items-center">
-          <div className="me-2 ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              class="bi bi-search mt-1"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
-          </div> 
-          <div className=" w-100">
-            <input
-              type="text"
-              name="location"
-              onChange={handleChange}
-              className="inputBox body-text box-shadow mt-2"
-              placeholder="Search job by city or location..."
-              list="locations"
-              defaultValue={query.location}
-            />
-          </div>
+        <div className=" w-100">
+          <input
+            type="text"
+            name="location"
+            onChange={handleChange}
+            className="inputBox body-text box-shadow mt-2"
+            placeholder="Search job by city or location..."
+            list="locations"
+            defaultValue={query.location}
+          />
         </div>
       </p>
       <div className="collapse text-dark bg-white p-2 pt-0 rounded" id="filter">
