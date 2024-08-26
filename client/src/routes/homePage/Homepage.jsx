0fb74055700f10b7a8dcdecbfdb49f1b987829f4
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-26 19:40:36 +0530

    gfjgh

--- client/src/routes/homePage/Homepage.jsx
+++ client/src/routes/homePage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./homePage.css";
 import homeImg from "../../assets/lifting.svg";
 import Input from "../../components/input/input";
