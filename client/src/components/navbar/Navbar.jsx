d7bfdacaead7e6989cdcf1d4cc88605b39a4063a
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-26 08:06:26 +0530

    grhrt

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -37,10 +37,10 @@ const Navbar = () => {
       otpRef.current.value = "";
       btn.current.click();
     }
-    if (btn.current && newUser) {
+    if (btn.current && !newUser) {
       btn.current.click();
     }
-  }, [newUser, user]); // This effect runs when `newUser` changes
+  }, [newUser, localStorage.getItem("user")]); // This effect runs when `newUser` changes
 
   const isActive = (path) => {
     return location.pathname === path ? "active" : "";
