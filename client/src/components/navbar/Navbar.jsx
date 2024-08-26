47e2b695b9bff974ebad0088f59fb51195b01cfe
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-26 09:03:02 +0530

    uklhl

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -39,7 +39,6 @@ const Navbar = () => {
     if (btn.current && !newUser) {
       btn.current.click();
     }
-    setNewUser(false)
   }, [newUser, localStorage.getItem("user")]); // This effect runs when `newUser` changes
 
   const isActive = (path) => {
