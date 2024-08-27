6d064ff5460096ef21e7d7142cd2f08ea7c00869
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-27 10:50:44 +0530

    gfigkg

--- client/src/routes/singlepage/SinglePage.jsx
+++ client/src/routes/singlepage/SinglePage.jsx
@@ -133,7 +133,17 @@ const SinglePage = () => {
                 currentUser && post.caterId === currentUser.id ? "" : "d-none"
               }`}
             >
-              Delete Post
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-trash"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+              </svg>
             </button>
           </div>
           <div className="row">
@@ -179,7 +189,7 @@ const SinglePage = () => {
             </div>
           </div>
           <div className="eventDesc fs-normal">
-            <p className="p-0 mb-2" >{post?.description}</p> 
+            <p className="p-0 mb-2">{post?.description}</p>
             <div className=" fs-normal mb-2">
               <b>Address</b> : {post?.address}
               <br />
@@ -188,7 +198,7 @@ const SinglePage = () => {
             </div>
           </div>
 
-          <div className="extra d-flex gap-md-2 gap-1 flex-wrap my-2"> 
+          <div className="extra d-flex gap-md-2 gap-1 flex-wrap my-2">
             <div className="workingHrs bg-text small-text p-2 rounded">
               Date : <b>{post?.startDate} </b>
             </div>
@@ -213,7 +223,9 @@ const SinglePage = () => {
             </div>
             <div className="col-md-3 col-12">
               <div className="join-btn">
-                <button className="btn btn-primary w-100 mb-1">I'm Interested</button>
+                <button className="btn btn-primary w-100 mb-1">
+                  I'm Interested
+                </button>
               </div>
             </div>
           </div>
