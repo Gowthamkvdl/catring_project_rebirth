d0b78e783c23ccc8da09f57503ec1ba285ad1391
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-30 20:14:07 +0530

    xbdfn

--- client/src/routes/singlepage/SinglePage.jsx
+++ client/src/routes/singlepage/SinglePage.jsx
@@ -207,7 +207,7 @@ const SinglePage = () => {
                 >
                   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
                 </svg>
-                <span className="text-dark body-text fw-medium">
+                <span className="text-dark subtitle-text fs-3 fw-medium">
                   {post?.city}
                 </span>
               </div>
