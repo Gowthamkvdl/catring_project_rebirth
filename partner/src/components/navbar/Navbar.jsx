aa1e715581fc3ba87820051bf14adb6ffbd9690d
gowthamkvdl <gowthamkvdl@gmail.com>
2024-09-17 08:35:44 +0530

    rtujtru

--- partner/src/components/navbar/Navbar.jsx
+++ partner/src/components/navbar/Navbar.jsx
@@ -91,6 +91,16 @@ const Navbar = () => {
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem("partnerID");
+    localStorage.removeItem("user");
+    updateUser(null);
+    window.location.reload();
+    toast.success("Logout Successful", {
+      id: "logout successful",
+    })
+  }
+
   return (
     <div>
       <nav
@@ -247,6 +257,37 @@ const Navbar = () => {
                       className={`nav-link text-decoration-none d-none d-sm-block body-text ${isActive(
                         "/servers-profiles"
                       )}`}
+                    >
+                      Servers
+                    </div>
+                  </li>
+                </Link>
+                <Link onClick={handleLogout}>
+                  <li className={`nav-item px-3 `}>
+                    <div
+                      className={`d-block d-sm-none phone-nav-item d-flex justify-content-center align-items-center flex-column`}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28.5"
+                        height="28.5"
+                        fill="currentColor"
+                        class="bi bi-box-arrow-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                        />
+                      </svg>
+                      <p className="text-center small-text mb-1">Logout</p>
+                    </div>
+                    <div
+                      className={`nav-link text-decoration-none d-none d-sm-block body-text `}
                     >
                       Servers
                     </div>
