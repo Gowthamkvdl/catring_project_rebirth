7ec4ecc3106941831e64dc5555086b29c0a2dea2
Gowtham K <gowthamkvdl@gmail.com>
2025-09-18 08:04:18 -0700

    hgfngf

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -640,7 +640,7 @@ const Navbar = () => {
                           renderInput={(props) => (
                             <input
                               {...props}
-                              className="form-control form-control-xl mx-3"
+                              className="form-control form-control-xl mx-1"
                               style={{
                                 color: "black",
                                 fontSize: "24px",
